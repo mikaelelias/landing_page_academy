25f16eaa197f08084f4edc6216e1e5dc332e6b47
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-08 21:44:18 -0300

    update JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+function agenda() {
+    var htmlElement = document.querySelector('.formulario_aula');
+    htmlElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest"})
+}
+
+function pacotes() {
+    var htmlElement = document.querySelector('.contato');
+    document.documentElement.scrollTop += 100;
+}   
